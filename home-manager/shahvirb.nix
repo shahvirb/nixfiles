39919ffc8047675a75ed786951c6553497876c3c
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-04-06 15:08:01 -0500

    Fixing formatting

--- home-manager/shahvirb.nix
+++ home-manager/shahvirb.nix
@@ -1,94 +1,94 @@
- { config, pkgs, ... }:
- 
- {
-    home.stateVersion = "23.11";
+{ config, pkgs, ... }:
 
-    home.packages = with pkgs; [
-      discord
-      firefox
-      git-credential-oauth
-      spotify
-      sublime4
-      vscode
-    ];
+{
+  home.stateVersion = "23.11";
 
-    nixpkgs.config.allowUnfree = true;
-    nixpkgs.config.permittedInsecurePackages = [
-      "openssl-1.1.1w"
-    ];
- 
-    programs.bash = {
-      enable = true;
-      shellAliases = {
-        nrbb = "sudo nixos-rebuild boot";
-        nrbs = "sudo nixos-rebuild switch";
-      };
+  home.packages = with pkgs; [
+    discord
+    firefox
+    git-credential-oauth
+    spotify
+    sublime4
+    vscode
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrbb = "sudo nixos-rebuild boot";
+      nrbs = "sudo nixos-rebuild switch";
     };
+  };
 
-    programs.git = {
-      enable = true;
-      extraConfig = {
-        credential.helper = "oauth";
-      };
-      userName = "Shahvir Buhariwalla";
-      userEmail = "shahvirb@gmail.com";
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      credential.helper = "oauth";
     };
+    userName = "Shahvir Buhariwalla";
+    userEmail = "shahvirb@gmail.com";
+  };
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-    programs.firefox = {
-      enable = true;
-      profiles = {
-        default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          settings = {
-            "browser.startup.homepage" = "http://helium:8992";
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.order.1" = "DuckDuckGo";
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        settings = {
+          "browser.startup.homepage" = "http://helium:8992";
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
 
-            # "widget.use-xdg-desktop-portal.file-picker" = 1;
-            "browser.aboutConfig.showWarning" = false;
-            # "browser.compactmode.show" = true;
-            "browser.cache.disk.enable" = false; # Be kind to hard drive
+          # "widget.use-xdg-desktop-portal.file-picker" = 1;
+          "browser.aboutConfig.showWarning" = false;
+          # "browser.compactmode.show" = true;
+          "browser.cache.disk.enable" = false; # Be kind to hard drive
 
-            # "widget.disable-workspace-management" = true;
-          };
-          search = {
-            force = true;
-            default = "DuckDuckGo";
-            order = [ "DuckDuckGo" "Google" ];
-          };
+          # "widget.disable-workspace-management" = true;
+        };
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = [ "DuckDuckGo" "Google" ];
         };
       };
     };
+  };
 
-    wayland.windowManager.hyprland = {
-      enable = true;
-      settings = {
-        "$mod" = "SUPER";
-        bind =
-          [
-            "$mod, F, exec, firefox"
-          ]
-          ++ (
-            # workspaces
-            # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-            builtins.concatLists (builtins.genList (
-                x: let
-                  ws = let
-                    c = (x + 1) / 10;
-                  in
-                    builtins.toString (x + 1 - (c * 10));
-                in [
-                  "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-                ]
-              )
-              10)
-          );
-      };
-
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, F, exec, firefox"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
     };
-  }
+
+  };
+}
