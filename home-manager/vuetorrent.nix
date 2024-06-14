c51f8641568d2dd7e6597191e2b03ce85711f0cf
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-06-14 08:05:27 -0500

    Adding vuetorrent install

---
+++ home-manager/vuetorrent.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+let
+  vtOverlay = builtins.fetchGit "https://git.kempkens.io/daniel/nix-overlay.git";
+  vuetorrent = import "${vtOverlay}/packages/vuetorrent.nix" {
+    inherit pkgs;
+    lib = pkgs.lib;
+  };
+in
+{
+  home.packages = with pkgs; [
+    vuetorrent
+  ];
+
+  systemd.user.tmpfiles.rules = [
+    "L ${config.home.homeDirectory}/vuetorrent - - - - ${vuetorrent}/share/public"
+  ];
+}
--- hosts/mediaserver2/configuration.nix
+++ hosts/mediaserver2/configuration.nix
@@ -34,7 +34,7 @@ in
       imports = [
         ../../home-manager/shahvirb.nix
         ../../home-manager/mediaserver2.nix
-        ../../home-manager/python.nix
+        ../../home-manager/vuetorrent.nix
       ];
     };
 
