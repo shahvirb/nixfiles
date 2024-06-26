f8ccfa2428f9cf315b35305d80d5c4c9c948f554
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-06-26 16:52:57 -0500

    fixing smb module

--- modules/smb-openmediavault-mediaauthor.nix
+++ modules/smb-openmediavault-mediaauthor.nix
@@ -1,14 +1,18 @@
 { config, pkgs, ... }:
 {
-  # For mount.cifs, required unless domain name resolution is not needed.
-  environment.systemPackages = [ pkgs.cifs-utils ];
+  environment.systemPackages = [ pkgs.samba4Full ];
   fileSystems."/mnt/Media" = {
-    device = "//openmediavault/Media";
+    device = "//openmediavault.home.lab/Media";
     fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    # options = let
+    #   automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    # in ["${automount_opts},credentials=/etc/nixos/omv-mediaauthor.secrets,vers=3.1.1,uid=1000,gid=1000"];
+    options = ["_netdev,***REMOVED***,password=***REMOVED***,vers=3.1.1,uid=1000,gid=1000,noauto,x-systemd.automount,x-systemd.idle-timeout=3600"];
+  };
 
-    in ["${automount_opts},username=mediauthor,password=***REMOVED***"];
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
   };
 }
