4ef511af4c2b0d39d88352852e26f48ac0da39f9
shahvirb <shahvirb@gmail.com>
2024-12-07 15:10:36 -0600

    remove home manager import in configuration.nix

--- hosts/argon/configuration.nix
+++ hosts/argon/configuration.nix
@@ -4,10 +4,6 @@
 
   imports = [
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
-    (import "${builtins.fetchTarball {
-      url = "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
-      sha256 = "00wp0s9b5nm5rsbwpc1wzfrkyxxmqjwsc1kcibjdbfkh69arcpsn";
-    }}/nixos")
     ../../modules/common.nix
     ../../modules/1password.nix
     ../../modules/sshkeys.nix
