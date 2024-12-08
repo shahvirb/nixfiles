787804a4a6f673445a7dc6bc50043cd9c248f095
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-12-08 12:03:31 -0600

    cleaning up unused items in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,30 +12,6 @@
         email = "shahvirb@gmail.com";
         gitUserName = "shahvirb";
         gitUserEmail = "shahvirb@gmail.com";
-      #   dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
-      #   theme = "uwunicorn-yt"; # selcted theme from my themes directory (./themes/)
-      #   wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
-      #   # window manager type (hyprland or x11) translator
-      #   wmType = if (wm == "hyprland") then "wayland" else "x11";
-      #   browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
-      #   defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
-      #   term = "alacritty"; # Default terminal command;
-      #   font = "Intel One Mono"; # Selected font
-      #   fontPkg = pkgs.intel-one-mono; # Font package
-      #   editor = "emacsclient"; # Default editor;
-      #   # editor spawning translator
-      #   # generates a command that can be used to spawn editor inside a gui
-      #   # EDITOR and TERM session variables must be set in home.nix or other module
-      #   # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
-      #   spawnEditor = if (editor == "emacsclient") then
-      #                   "emacsclient -c -a 'emacs'"
-      #                 else
-      #                   (if ((editor == "vim") ||
-      #                        (editor == "nvim") ||
-      #                        (editor == "nano")) then
-      #                          "exec " + term + " -e " + editor
-      #                    else
-      #                      editor);
       };
 
 
@@ -63,14 +39,6 @@
         };
       };
 
-      # pkgs-emacs = import inputs.emacs-pin-nixpkgs {
-      #   system = systemSettings.system;
-      # };
-
-      # pkgs-kdenlive = import inputs.kdenlive-pin-nixpkgs {
-      #   system = systemSettings.system;
-      # };
-
       # configure lib
       # use nixpkgs if running a server (lxc profile)
       # otherwise use patched nixos-unstable nixpkgs
@@ -87,17 +55,6 @@
                inputs.home-manager-stable
              else
                inputs.home-manager-unstable);
-
-      # Systems that can run tests:
-      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-
-      # Function to generate a set based on supported systems:
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-      # Attribute set of nixpkgs for each system:
-      nixpkgsFor =
-        forAllSystems (system: import inputs.nixpkgs { inherit system; });
-
     in {
       homeConfigurations = {
         user = home-manager.lib.homeManagerConfiguration {
@@ -130,101 +87,16 @@
           };
         };
       };
-
-      # packages = forAllSystems (system:
-      #   let pkgs = nixpkgsFor.${system};
-      #   in {
-      #     default = self.packages.${system}.install;
-
-      #     install = pkgs.writeShellApplication {
-      #       name = "install";
-      #       runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
-      #       text = ''${./install.sh} "$@"'';
-      #     };
-      #   });
-
-      # apps = forAllSystems (system: {
-      #   default = self.apps.${system}.install;
-
-      #   install = {
-      #     type = "app";
-      #     program = "${self.packages.${system}.install}/bin/install";
-      #   };
-      # });
     };
 
   inputs = {
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
-    # emacs-pin-nixpkgs.url = "nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6";
-    # kdenlive-pin-nixpkgs.url = "nixpkgs/cfec6d9203a461d9d698d8a60ef003cac6d0da94";
-
     home-manager-unstable.url = "github:nix-community/home-manager/master";
     home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     home-manager-stable.url = "github:nix-community/home-manager/release-24.05";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
-
-    # nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
-    # nix-doom-emacs.inputs.nixpkgs.follows = "emacs-pin-nixpkgs";
-
-    # nix-straight.url = "github:librephoenix/nix-straight.el/pgtk-patch";
-    # nix-straight.flake = false;
-    # nix-doom-emacs.inputs.nix-straight.follows = "nix-straight";
-
-  #   eaf = {
-  #     url = "github:emacs-eaf/emacs-application-framework";
-  #     flake = false;
-  #   };
-  #   eaf-browser = {
-  #     url = "github:emacs-eaf/eaf-browser";
-  #     flake = false;
-  #   };
-  #   org-nursery = {
-  #     url = "github:chrisbarrett/nursery";
-  #     flake = false;
-  #   };
-  #   org-yaap = {
-  #     url = "gitlab:tygrdev/org-yaap";
-  #     flake = false;
-  #   };
-  #   org-side-tree = {
-  #     url = "github:localauthor/org-side-tree";
-  #     flake = false;
-  #   };
-  #   org-timeblock = {
-  #     url = "github:ichernyshovvv/org-timeblock";
-  #     flake = false;
-  #   };
-  #   org-krita = {
-  #     url = "github:librephoenix/org-krita";
-  #     flake = false;
-  #   };
-  #   org-xournalpp = {
-  #     url = "gitlab:vherrmann/org-xournalpp";
-  #     flake = false;
-  #   };
-  #   org-sliced-images = {
-  #     url = "github:jcfk/org-sliced-images";
-  #     flake = false;
-  #   };
-  #   phscroll = {
-  #     url = "github:misohena/phscroll";
-  #     flake = false;
-  #   };
-  #   mini-frame = {
-  #     url = "github:muffinmad/emacs-mini-frame";
-  #     flake = false;
-  #   };
-
-  #   stylix.url = "github:danth/stylix";
-
-  #   rust-overlay.url = "github:oxalica/rust-overlay";
-
-    # blocklist-hosts = {
-    #   url = "github:StevenBlack/hosts";
-    #   flake = false;
-    # };
   };
 }
