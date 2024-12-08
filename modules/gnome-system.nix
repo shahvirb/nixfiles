4d2b20afb153f22b554db01eb3ee26a0ed6f161b
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-12-08 13:46:24 -0600

    addressing breakage with gnome-settings-daemon

--- modules/gnome-system.nix
+++ modules/gnome-system.nix
@@ -22,7 +22,7 @@
 
   # For systray icons
   environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
 
   environment.gnome.excludePackages = (with pkgs; [
     atomix # puzzle game
