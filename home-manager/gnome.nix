b4ba613d735b95dbe41199311c86ed8203247ea0
shahvirb <shahvirb@gmail.com>
2024-12-13 15:57:24 -0600

    switching to use paperwm

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -1,15 +1,7 @@
-{ config, lib, pkgs, ... }:
-with lib;
-let
-  cfg = config.services.my-gnome;
-in
-{
-  options.services.my-gnome = {
-    enableDashToPanel = mkEnableOption "Enable dash-to-panel gnome extension";
-  };
+{ config, pkgs, ... }:
 
+{
   config = {
-    services.my-gnome.enableDashToPanel = mkDefault true;
 
     # Read https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/ for a good procedure on dconf watch
     dconf.settings = {
@@ -20,13 +12,13 @@ in
       "org/gnome/shell" = {
         disable-user-extensions = false;
 
-        # `gnome-extensions list` for a list
+        # Run "gnome-extensions list" for a list
         enabled-extensions = [
+          "paperwm@paperwm.github.com"
           "appindicatorsupport@rgcjonas.gmail.com"
-          "gTile@vibou"
-          "user-theme@gnome-shell-extensions.gcampax.github.com"
           "Vitals@CoreCoding.com"
-        ] ++ (if cfg.enableDashToPanel then ["dash-to-panel@jderose9.github.com"] else []);
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+        ];
       };
 
       "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -75,6 +67,7 @@ in
       gnome-tweaks
       gnomeExtensions.gtile
       gnomeExtensions.vitals
-    ] ++ (if cfg.enableDashToPanel then [gnomeExtensions.dash-to-panel] else []);
+      gnomeExtensions.paperwm
+    ];
   };
 }
