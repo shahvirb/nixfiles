96f494d7a1d2c7c4d2e0f38c56354b86517fcabf
shahvirb <shahvirb@gmail.com>
2024-12-17 14:10:05 -0600

    TODO done

--- modules/1password.nix
+++ modules/1password.nix
@@ -7,6 +7,5 @@
     # Certain features, including CLI integration and system authentication support,
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "${userSettings.username}" ];
-    # TODO this should not be a hardcoded username
   };
 }
