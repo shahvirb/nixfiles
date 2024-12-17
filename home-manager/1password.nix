c5eb3326af6ec4397c2feff81872c21da19f4e91
shahvirb <shahvirb@gmail.com>
2024-12-17 14:18:51 -0600

    set 1password to autostart on logon

--- home-manager/1password.nix
+++ home-manager/1password.nix
@@ -14,4 +14,15 @@
           IdentityAgent ~/.1password/agent.sock
     '';
   };
+
+  # Autostart 1Password GUI on login
+  home.file.".config/autostart/1password.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=1Password
+    Exec=1password
+    Icon=1password
+    Comment=Password Manager
+    X-GNOME-Autostart-enabled=true
+  '';
 }
--- modules/1password.nix
+++ modules/1password.nix
@@ -8,4 +8,7 @@
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "${userSettings.username}" ];
   };
+
+  # We'll rely on autostart in home-manager/1password.nix to always start it on logon
+  xdg.autostart.enable = true;
 }
