8bc30bc4bbabd529a34d276e49f7e641e3872d79
shahvirb <shahvirb@gmail.com>
2025-01-17 18:46:31 -0600

    opening ports for the unifi controller

--- hosts/argon/configuration.nix
+++ hosts/argon/configuration.nix
@@ -16,5 +16,11 @@
     manageHostName = false;
   };
 
+  # For the Unifi controller
+  networking.firewall = {
+    allowedTCPPorts = [ 6789 8080 8081 8443 8843 8880 ];
+    allowedUDPPorts = [ 3478 10001 ];
+  };
+
   system.stateVersion = "23.11";
 }
