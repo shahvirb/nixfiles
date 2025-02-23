4562df0915bc9a9be90013b2bc0d942c360e3496
shahvirb <shahvirb@gmail.com>
2025-02-23 09:25:38 -0600

    adding tandoor recipes

--- hosts/argon/configuration.nix
+++ hosts/argon/configuration.nix
@@ -17,12 +17,15 @@
   };
 
   networking.firewall = lib.mkMerge [
+    { # homepage
+      allowedTCPPorts = [ 8992 ];
+    }
+    { # tandoor
+      allowedTCPPorts = [ 8014 ];
+    }
     { # unifi controller
       allowedTCPPorts = [ 6789 8087 8443 8843 8880 ];
       allowedUDPPorts = [ 1900 3478 5514 10001 ];
-    }
-    { # homepage
-      allowedTCPPorts = [ 8992 ];
     }
   ];
 
